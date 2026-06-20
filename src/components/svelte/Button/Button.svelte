d21e106579dc7c56ca91a189a813b9173ac6e8fd
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-06-20 15:37:21 +0200

    add: svelte integration; first svelte components; component islands

---
+++ src/components/svelte/Button/Button.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { text } from "./dummyData.json";
+  import Text from "../Text/Text.svelte";
+
+  // let name = 'Hello World :-)';
+  // if (text) name = text;
+
+  let counter = $state(0);
+
+  function handleClick() {
+    // alert('Svelte Button clicked!');
+    counter++;
+  }
+</script>
+
+<button class="btn" onclick={handleClick}>{text.toUpperCase()}<Text></Text><span>{counter}</span><span>{counter === 0 || counter === 1 ? "Time" : "Times"} </span></button>
+
+<style>
+  .btn {
+    display: flex;
+    gap: 15px;
+    padding: 5px;
+    justify-content: center;
+    align-items: center;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 4px;
+  }
+
+  .btn:hover {
+    background-color: #0056b3;
+  }
+
+  span {
+    color: goldenrod;
+    font-family:
+      Comic Sans MS,
+      cursive;
+    font-size: 1.2em;
+  }
+</style>
---
+++ src/components/svelte/Text/Text.svelte
@@ -0,0 +1,13 @@
+<script>
+let text = 'Das ist eine TextComponente :-)';
+</script>
+
+<span>{text}</span>
+
+<style>
+    span {
+        color: rebeccapurple;
+        font-family: Comic Sans MS, cursive;
+        font-size: 1.2em;
+    }
+</style>
