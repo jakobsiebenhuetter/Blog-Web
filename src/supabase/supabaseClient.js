4c47b446f9010f1e513c875a042202dd4c19384e
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-07-27 09:59:29 +0200

    add

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const PUBLIC_SUPABASE_URL = "https://tdnsixpaxjkqjnzwghbf.supabase.co"
+const PUBLIC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkbnNpeHBheGprcWpuendnaGJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2MDM0MTgsImV4cCI6MjA5ODE3OTQxOH0.KanJWl3xX01STH0-jvKCwUa-kEjHh4xozWW5oQnfNUY";
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 export default supabase;
