dee5dad57b835d7a4491fc11a353b9ef5d7df178
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-07-29 16:52:23 +0200

    fix: css loading for svelte components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
 
-  integrations: [svelte({emitCss: false})],
+  integrations: [svelte()],
 
 
   
